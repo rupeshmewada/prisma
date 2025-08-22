5b40a77698bcd64091ca70e9da938d2842c0bcd0
rupeshmewada <rupesh.mewada5@gmail.com>
2025-08-22 19:20:21 +0530

    crud in prisma

--- index.js
+++ index.js
@@ -1,38 +1,87 @@
-import { PrismaClient } from './generated/prisma/index.js'
+import { PrismaClient } from "./generated/prisma/index.js";
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
-// await prisma.user.create({
-//     data: {
-//       name: 'Alice',
-//       email: 'alice@prisma.io',
-//     },
-//   })
 
+  //   create a user
+  //   const adduser = await prisma.user.create({
+  //     data: {
+  //       name: "ram",
+  //       email: "ram@gmail.com",
+  //     },
+  //   });
+  //   console.log(adduser);
 
-  const allUsers = await prisma.user.findMany({  })
-  console.dir(allUsers)
+  //   create multiple user
 
+  //   const addusers = await prisma.user.createMany({
+  //     data: [
+  //       {
+  //         name: "arun",
+  //         email: "arun@gmail.com",
+  //       },
+  //       {
+  //         name: "shyam",
+  //         email: "shyam@gmail.com",
+  //       },
+  //     ],
+  //   });
+  //   console.log(addusers);
+
+  // fetch all data
+  //   const allUsers = await prisma.user.findMany({});
+  //   console.dir(allUsers);
+
+  //   // fetch first data
+  //   const allUsers = await prisma.user.findFirst({});
+  //   console.dir(allUsers);
+
+  //   // fetch unique data
+  //   const allUsers = await prisma.user.findUniqueOrThrow({
+  //     where: { id: 4 },
+  //   });
+  //   console.dir(allUsers);
+
+  //   // updata data
+  //   const updateUser = await prisma.user.update({
+  //     where: { id: 1 },
+  //     data: {
+  //       name: "ram1",
+  //       email: "ram1@gmail1.com",
+  //     },
+  //   });
+  //   console.log(updateUser);
+
+  // updata many data
+  //    const updateUser = await prisma.user.updateMany({
+  //     where: { email: "ram1@gmail1.com" },
+  //     data: {
+  //       name: "ram"
+  //     },
+  //   });
+  //   console.log(updateUser);
+
+  // delete user
+//   const deluser = await prisma.user.delete({
+//     where: { id: 4 },
+//   });
+
+  // delete user
+//   const deluser = await prisma.user.delete({
+//     where: { id: 4 },
+//   });
 }
-
-// git remote add origin git@github.com:vsgeek/KCHrPortal.git
-// git branch -M main
-// git push -u origin main
-
-// git@github.com:rupeshmewada/prisma.git
-
 
 
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
